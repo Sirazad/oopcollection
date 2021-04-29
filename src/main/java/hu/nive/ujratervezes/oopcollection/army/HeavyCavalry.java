70feb9de54c3647447ccec6a1647929aaca75ac0
Sirazad <lajosimola2@gmail.com>
2021-04-29 10:52:46 +0200

    pass 6 tests

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/HeavyCavalry.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/HeavyCavalry.java
@@ -1,9 +1,18 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
 public class HeavyCavalry extends MilitaryUnit {
+    private boolean firstAttack = true;
 
     HeavyCavalry() {
         super(150, 20, true);
     }
-
+    @Override
+    public int doDamage() {
+        if (firstAttack){
+            firstAttack = false;
+            return super.doDamage() * 3;
+        } else {
+            return super.doDamage();
+        }
+    }
 }
